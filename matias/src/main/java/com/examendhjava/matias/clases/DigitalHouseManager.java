456bd866109152895e163b07c622ef7216a7b804
MatiasF <matiasferrer@live.com>
2020-04-22 21:53:21 -0300

    Clase DigitalHouseManager y UML actualizado

---
+++ matias/src/main/java/com/examendhjava/matias/clases/DigitalHouseManager.java
@@ -0,0 +1,12 @@
+package com.examendhjava.matias.clases;
+
+import java.util.ArrayList;
+
+public class DigitalHouseManager {
+
+	private ArrayList<Alumno> listaAlumnos;
+	private ArrayList<Profesor> listaProfesores;
+	private ArrayList<Curso> listaCursos;
+	private ArrayList<Inscripcion> listaInscriptos;
+
+}
